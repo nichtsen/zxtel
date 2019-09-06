cae523cd4a82d774428e54de7b03518d8ef903ad
nichtsen <hobblong@163.com>
2019-09-06 11:41:40 +0800

    batch

--- tel.bat
+++ tel.bat
@@ -1,5 +1,15 @@
 @echo off
+:telCheck
+if not exist c:/windows/system32/telnet.exe (
+echo "telnet is not available(continue to install)"
+pause
+pkgmgr /iu:"TelnetClient"
+goto:telCheck
+) ELSE echo "telnet is available"
+:telnet
+pause
 telnet 202.99.22.74 30466
 pause
 telnet 219.142.124.205 30466
+echo "sucessfully telnet!"
 pause
